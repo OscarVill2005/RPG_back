9ba76e4283f719420882f1ea2696396a1fd6aa87
Aaron Clar <aclar05@colegiosantamonica.eu>
2025-06-02 14:56:38 +0200

    cambios players

--- src/index.ts
+++ src/index.ts
@@ -68,58 +68,88 @@ io.on('connection', (socket: any) => {
             let user_emails = Array.from(emails[info.code]);
             let users_data = []
 
-            for(let i = 0; i < user_emails.length; i++) {
-            try {
-                let query = `SELECT * FROM players WHERE id='${user_emails[i]}'`;
-                let db_response = await db.query(query);
+            for (let i = 0; i < user_emails.length; i++) {
+                try {
+                    let query = `SELECT * FROM players WHERE id='${user_emails[i]}'`;
+                    let db_response = await db.query(query);
 
-                if (db_response.rows.length > 0) {
-                    console.log(`Usuario encontrado: ${db_response.rows[0].id}`);
-                    users_data.push(db_response.rows[0])
-                } else {
-                    console.log(`Player not found.`)
+                    if (db_response.rows.length > 0) {
+                        console.log(`Usuario encontrado: ${db_response.rows[0].id}`);
+                        users_data.push(db_response.rows[0])
+                    } else {
+                        console.log(`Player not found.`)
+                    }
+
+                } catch (err) {
+                    console.error(err);
                 }
-
-            } catch (err) {
-                console.error(err);
             }
-        }
 
-        let gamedata = {
-            //añadir toda la info de los players boss y turno
-            player1: {
-                name: users_data[0].name
-            },
-            player2: {
-                name: users_data[1].name
-            },
-            player3: {
-                name: users_data[2].name
-            },
-            player4: {
-                name: users_data[3].name
-            },
-            boss: {
+            let gamedata = {
+                //añadir toda la info de los players boss y turno
+                player1: {
+                    name: users_data[0].name,
+                    health_points: users_data[0].health_points,
+                    mana_points: users_data[0].mana_points,
+                    strength: users_data[0].strength,
+                    magical_damage: users_data[0].magical_damage,
+                    critical_chance: users_data[0].critical_chance,
+                    critical_damage: users_data[0].critical_damage,
+                    defense: users_data[0].defense,
 
-            },
-            game:{
-                current_turn: 1,
-                game_over: false,
+                },
+                player2: {
+                    name: users_data[1].name,
+                    health_points: users_data[1].health_points,
+                    mana_points: users_data[1].mana_points,
+                    strength: users_data[1].strength,
+                    magical_damage: users_data[1].magical_damage,
+                    critical_chance: users_data[1].critical_chance,
+                    critical_damage: users_data[1].critical_damage,
+                    defense: users_data[1].defense,
+                },
+                player3: {
+                    name: users_data[2].name,
+                    health_points: users_data[2].health_points,
+                    mana_points: users_data[2].mana_points,
+                    strength: users_data[2].strength,
+                    magical_damage: users_data[2].magical_damage,
+                    critical_chance: users_data[2].critical_chance,
+                    critical_damage: users_data[2].critical_damage,
+                    defense: users_data[2].defense,
+                },
+                player4: {
+                    name: users_data[3].name,
+                    health_points: users_data[3].health_points,
+                    mana_points: users_data[3].mana_points,
+                    strength: users_data[3].strength,
+                    magical_damage: users_data[3].magical_damage,
+                    critical_chance: users_data[3].critical_chance,
+                    critical_damage: users_data[3].critical_damage,
+                    defense: users_data[3].defense,
+                },
+                boss: {
+                    
+
+                },
+                game: {
+                    current_turn: 1,
+                    game_over: false,
+                }
             }
-        }
 
-        socket.emit('game started' + info.code, gamedata);
+            socket.emit('game started' + info.code, gamedata);
 
-        socket.on('turn' + info.code, (turn_events: any) => {
-        //gestionar game data de turno
+            socket.on('turn' + info.code, (turn_events: any) => {
+                //gestionar game data de turno
 
-        socket.emit('finished_turn' + info.code, gamedata)
+                socket.emit('finished_turn' + info.code, gamedata)
+            })
+
         })
-        
-    })
 
-});
-    
+    });
+
 
 });
 
