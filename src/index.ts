b305b6101306018290a5d12ddc244ee4ec6fd3bf
OscarEmpirico <theoscardream@outlook.es>
2025-06-05 11:31:34 +0200

    logica hecha, falta q cuando pierdas game over true y los ataques

--- src/index.ts
+++ src/index.ts
@@ -63,9 +63,10 @@ io.on('connection', (socket: any) => {
         io.emit('user_list_' + info.code, Array.from(users[info.code]));
         console.log(users)
 
-        socket.on('start game' + info.code, async (start: boolean) => {
-
-            let user_emails = Array.from(emails[info.code]);
+        socket.on('start game' + info.code , async (user_list: any) => {
+            console.log('Esto es info en back:' + JSON.stringify(user_list))
+            let user_emails = user_list;
+            console.log('USER EMAILS:' + user_emails);
             let users_data = []
 
             for (let i = 0; i < user_emails.length; i++) {
@@ -84,7 +85,7 @@ io.on('connection', (socket: any) => {
                     console.error(err);
                 }
             }
-
+            console.log('USERSDATA:' + users_data)
             let gamedata = {
                 //añadir toda la info de los players boss y turno
                 player1: {
@@ -99,37 +100,37 @@ io.on('connection', (socket: any) => {
 
                 },
                 player2: {
-                    name: users_data[1].name,
-                    health_points: users_data[1].health_points,
-                    mana_points: users_data[1].mana_points,
-                    strength: users_data[1].strength,
-                    magical_damage: users_data[1].magical_damage,
-                    critical_chance: users_data[1].critical_chance,
-                    critical_damage: users_data[1].critical_damage,
-                    defense: users_data[1].defense,
+                    name: users_data[1]?.name,
+                    health_points: users_data[1]?.health_points,
+                    mana_points: users_data[1]?.mana_points,
+                    strength: users_data[1]?.strength,
+                    magical_damage: users_data[1]?.magical_damage,
+                    critical_chance: users_data[1]?.critical_chance,
+                    critical_damage: users_data[1]?.critical_damage,
+                    defense: users_data[1]?.defense,
                 },
                 player3: {
-                    name: users_data[2].name,
-                    health_points: users_data[2].health_points,
-                    mana_points: users_data[2].mana_points,
-                    strength: users_data[2].strength,
-                    magical_damage: users_data[2].magical_damage,
-                    critical_chance: users_data[2].critical_chance,
-                    critical_damage: users_data[2].critical_damage,
-                    defense: users_data[2].defense,
+                    name: users_data[2]?.name,
+                    health_points: users_data[2]?.health_points,
+                    mana_points: users_data[2]?.mana_points,
+                    strength: users_data[2]?.strength,
+                    magical_damage: users_data[2]?.magical_damage,
+                    critical_chance: users_data[2]?.critical_chance,
+                    critical_damage: users_data[2]?.critical_damage,
+                    defense: users_data[2]?.defense,
                 },
                 player4: {
-                    name: users_data[3].name,
-                    health_points: users_data[3].health_points,
-                    mana_points: users_data[3].mana_points,
-                    strength: users_data[3].strength,
-                    magical_damage: users_data[3].magical_damage,
-                    critical_chance: users_data[3].critical_chance,
-                    critical_damage: users_data[3].critical_damage,
-                    defense: users_data[3].defense,
+                    name: users_data[3]?.name,
+                    health_points: users_data[3]?.health_points,
+                    mana_points: users_data[3]?.mana_points,
+                    strength: users_data[3]?.strength,
+                    magical_damage: users_data[3]?.magical_damage,
+                    critical_chance: users_data[3]?.critical_chance,
+                    critical_damage: users_data[3]?.critical_damage,
+                    defense: users_data[3]?.defense,
                 },
                 boss: {
-                    health : Math.floor(Math.random() * (900 - 500 + 1 ) + 500 ),
+                    health : Math.floor(Math.random() * (900 - 400 + 1 ) + 400 ),
                     damage : Math.floor(Math.random() * (100 - 40 + 1 ) + 40 ),
                 },
                 game: {
@@ -143,11 +144,14 @@ io.on('connection', (socket: any) => {
             socket.emit('game started' + info.code, gamedata);
 
             socket.on('turn' + info.code, (action: any) => {
+                console.log('Turn emited')
                 console.log(action);
-                let user_emails = Array.from(emails[info.code]);
+                let user_emails = user_list;
+                console.log('USER EMAILS turn:' + user_emails)
                 //gestionar game data de turno
                 if (action.damage > 0){
                 gamedata.boss.health = gamedata.boss.health - action.damage;
+                console.log('VIDA BOSS :' + gamedata.boss.health);
                 }
                 if (action.heal > 0){
                     gamedata.player1.health_points = gamedata.player1.health_points + action.heal;
@@ -174,26 +178,32 @@ io.on('connection', (socket: any) => {
                 console.log(gamedata.game)
                 if(gamedata.boss.health <= 0){
                     gamedata.game.game_finished = true;
-                    socket.emit('finished_turn' + info.code, gamedata)
+                    socket.emit('finished_turn' + info, gamedata)
                 } else if (gamedata.player1.health_points <= 0 && gamedata.player2.health_points <= 0 && gamedata.player3.health_points <= 0 && gamedata.player4.health_points <= 0){
                     gamedata.game.game_over = true;
                     gamedata.game.game_finished = true;
-                    socket.emit('finished_turn' + info.code, gamedata)
+                    socket.emit('finished_turn' + info, gamedata)
                 } else {
                 if (gamedata.game.current_player == user_emails.length - 1){
+                    console.log('SOLO PLAYER')
                 gamedata.game.current_turn ++;
+                    console.log('TURNO EXTRA BOSS: ' + gamedata.game.current_turn);
                 gamedata.game.current_player = gamedata.game.current_turn % (user_emails.length + 1);
                 let boss_action = Math.floor(Math.random() * (5 - 1 + 1 ) + 1 )
+                console.log('Accion del boss:' + boss_action);
                 if (boss_action > 2){
                     gamedata.player1.health_points = gamedata.player1.health_points - (gamedata.boss.damage - (gamedata.player1.defense / 2));
                     gamedata.player2.health_points = gamedata.player2.health_points - (gamedata.boss.damage - (gamedata.player2.defense / 2));
                     gamedata.player3.health_points = gamedata.player3.health_points - (gamedata.boss.damage - (gamedata.player3.defense / 2));
                     gamedata.player4.health_points = gamedata.player4.health_points - (gamedata.boss.damage - (gamedata.player4.defense / 2));
+                    console.log('Vida de los jugadores:' + gamedata.player1.health_points)
                 } else {
                     gamedata.boss.health = gamedata.boss.health + Math.floor(Math.random() * (250 - 50 + 1 ) + 50 );
+                    console.log('Vida del boss:' + gamedata.boss.health);
                 }
                 }
-                socket.emit('finished_turn' + info.code, gamedata)
+                console.log('GAMEDATA end turn:' + JSON.stringify(gamedata))
+                socket.emit('finished_turn' + info, gamedata)
             }
             })
 
